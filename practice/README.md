305848fb91f5a3b522e8b959325c6fa88bc2a557
Robelyn Garcia <rog814@g.harvard.edu>
2019-09-15 18:15:10 -0700

    Initialized practice application

---
+++ practice/README.md
@@ -0,0 +1,9 @@
+# Project 1
++ By: *Robelyn Garcia Practice*
++ Production URL: <http://p1.yourdomain.com>
+
+## Outside resources
+*n/a*
+
+## Notes for instructor
+*n/a*
---
+++ practice/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang='en'>
+<head>
+	<title>Practice Application</title>
+	<meta charset='utf-8'>
+</head>
+<body>
+	<h1>Practice Application</h1>
+</body>
+</html>
