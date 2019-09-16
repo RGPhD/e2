21b61d784fa575ec1ce2416c221f86d42951ada6
Robelyn Garcia <rog814@g.harvard.edu>
2019-09-16 06:25:59 -0700

    Initialized p1 application

---
+++ p1/README.md
@@ -0,0 +1,9 @@
+# Project 1
++ By: *Robelyn Garcia*
++ Production URL: <http://p1.robelyngarcia.me>
+
+## Outside resources
++ My *brief bio* retrieved from <https://universityofsedona.com/rev-robelyn-garcia>
+
+## Notes for instructor
++ I used my local server and JSFiddle to work on and test my code <https://jsfiddle.net/RobelynGarcia/fe8jdhLs>
---
+++ p1/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+  <head>
+
+    <title>Robelyn Garcia</title>
+    <meta charset='utf-8'>
+
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <style>
+      section {
+        width: 750px;
+        margin: auto;
+        text-align: center;
+      }
+
+      h1 {
+        color: #cc0000;
+        text-align: center;
+        border-bottom: thick solid crimson;
+      }
+
+      img {
+        width: 250px;
+      }
+
+      p {
+        display: inline-block;
+        text-align: left;
+      }
+
+      article {
+        display: inline-block;
+        text-align: left;
+      }
+
+      footer {
+        margin: auto;
+        text-align: center;
+        color: #cc0000;
+        font-size: 110%;
+        border-top: thick solid #cc0000;
+      }
+
+    </style>
+
+  </head>
+
+  <body>
+    <header>
+      <h1>Robelyn Garcia, PhD</h1>
+    </header>
+    <section>
+
+      <img src='https://static.scholar.harvard.edu/files/styles/os_files_xxlarge/public/drrobelyngarcia/files/robelyn-garcia-harvard.jpg' alt='Photo of Robelyn Garcia'>
+
+      <h2>Brief Bio</h2>
+
+      <p>Dr. Robelyn Garcia has been a youth pastor, ordained minister and professor since the 1990s. She became a minister with International Metaphysical Ministry in 2018 while completing her ninth college degree, a Bachelors in Metaphysical Science, through the University of Metaphysics.</p>
+
+      <p>As a Multidisciplinary Post-Doc Scholar, Dr. Garcia’s mission is to educate and motivate! Her research transcends a myriad of disciplines. Through continued Metaphysical exploration, meditation research, implementation of real-world holistic health models, behavioral health coaching, spiritual counseling, telehealth modalities and biomimicry designs, her goal is to decrease the chronic and fatal health conditions associated with clinical obesity and geriatric depression. By utilizing and integrating the Metaphysical approach of self-improvement and self-development, Dr. Garcia creates a positive, nature-inspired wellness space for her clients and students. In her quest of a holistic approach for all endeavors, Dr. Garcia has began an educational journey as a Metaphysician with The University of Sedona and The University of Metaphysics.
+      </p>
+
+      <p>She is currently a Faculty Affiliate at Arizona State University, a Post-Doc Scholar at Harvard University Extension School, an American Red Cross Instructor at Senior University and a Private Professor at Personal Professors Online Academy. Dr. Garcia’s drive for educational excellence is fueled by the desire to help others with their overall physical, spiritual and behavioral health. She is also a former Professional Basketball Player, Kinesiology Professor, President Emeritus of the Jr. NBA-WNBA and Lifespan Professor. In 1996, after retiring from an All-Star professional championship basketball career, Dr. Garcia combined her love of the game with her passion for sharing the word of God as the Owner, Player and Coach for Christian Basketball Team Garcia.
+      </p>
+      <p>Dr. Garcia’s education is as diverse as her teaching. In addition to her 10 completed degrees, she just finished the coursework for her 2nd Doctorate in Behavioral Health at Arizona State University. She has also started her 11th college degree at Harvard University Extension School. The chronological break below reflects the years she spent as a Kinesiology Professor before beginning her pursuit to becoming a Multidisciplinary Professor, Health Educator and Interdisciplinary Post-Doc Scholar.
+      </p>
+
+      <article>
+        <h3>Her Degrees Include:</h3>
+        <ul>
+          <li>1985 – Associate of Arts & Sciences: Broadcasting & Kinesiology (Dodge City Comm College)</li>
+          <li>1990 – Bachelor of General Studies: Social & Health Sciences (Wichita State University)</li>
+          <li>1992 – Master of Science: Exercise & Sport Science (Central Missouri State University)</li>
+          <li>1993 – Master of Education in passing: Athletic Administration (LaSalle University)</li>
+          <li>1994 – Doctor of Philosophy: Education Administration (LaSalle University)</li>
+          <li>2009 – Post-Doc Graduate Certificate: Gerontology (Arizona State University)</li>
+          <li>2011 – Master of Science: Aging & Lifespan Development (Arizona State University)</li>
+          <li>2014 – Master of Arts: Criminal Justice & Criminology (Arizona State University)</li>
+          <li>2018 – Bachelor of Metaphysical Science (University of Metaphysics)</li>
+          <li>2019 – Graduate Certificate: Law Enforcement Administration (Arizona State University)</li>
+          <li>2020 – Doctor of Behavioral Health (<em>exp</em> at Arizona State University)</li>
+          <li>2020 – Professional Graduate Certificate: Web Technologies (<em>exp</em> at Harvard Extension School)</li>
+          <li>2021 – Master of Liberal Arts: Digital Media Design (<em>exp</em> at Harvard Extension School)</li>
+          <li>2021 – Master of Metaphysical Science (<em>exp</em> at University of Metaphysics)</li>
+          <li>2022 – Doctor of Metaphysical Science (<em>exp</em> at University of Sedona)</li>
+        </ul>
+      </article>
+    </section>
+    <br>
+    <footer>
+      <p><strong>Retrieved from <a href="https://universityofsedona.com/rev-robelyn-garcia" target="_blank" title="Click to visit University of Sedona">https://universityofsedona.com/rev-robelyn-garcia
+          </a></strong></p>
+    </footer>
+    <br>
+  </body>
+
+</html>
