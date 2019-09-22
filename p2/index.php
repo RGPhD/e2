e7f44ae73e9870d955244bb471fe94671cd048bf
Robelyn Garcia <rog814@g.harvard.edu>
2019-09-21 21:57:58 -0700

    Modified mechanics index.php file.

--- p2/index.php
+++ p2/index.php
@@ -24,20 +24,26 @@ $totalPlayer2 = ($make_value);
 <html lang='en'>
 <head>
 
-    <title>Trailblazers Knockout 3-Pt Shooting Game.</title>
+    <title>Trailblazers MVP Knockout 3-Pt Shooting Game.</title>
     <meta charset='utf-8'>
 
 <!-- Create external CSS file? -->
 <style>
 
       h1 {
-        color: blue;
+        color: #003399;
         text-align: center;
-        border-bottom: thick solid orange;
+        border-bottom: thick solid #FF6600;
       }
 
       h2 {
-        color: blue;
+        color: #003399;
+      }
+    
+    h3 {
+        color: #003399;
+        text-align: center;
+        font-size: 125%;
       }
 
       p {
@@ -52,8 +58,7 @@ $totalPlayer2 = ($make_value);
       .center {
   	display: block;
   	margin-left: auto;
-  	margin-right: auto;
-  	
+  	margin-right: auto;	
 	}
 
       footer {
@@ -61,7 +66,7 @@ $totalPlayer2 = ($make_value);
         text-align: center;
         font-size: 110%;
         font-weight: bold;
-        border-top: thick solid orange;
+        border-top: thick solid #FF6600;
       }
 
     </style>
@@ -71,25 +76,30 @@ $totalPlayer2 = ($make_value);
 <body>
 
 <header>
-<h1>Trailblazers Knockout 3-Pt Shooting Game</h1>
+<h1>Knockout 3-Pt Shooting Game <br>
+    Trailblazers MVP Version 1.16.15 beta</h1>
+<!-- Minimum Viable Product/Most Valuable Player --> 
 <img alt='WBCBL Trailblazers Basketball Player Logo' src='https://pbs.twimg.com/media/CBfUlbsVAAANjUG?format=jpg' class="center">
 </header>
 
 <h2>Mechanics</h2>
-<p>The "plain English" notes/plan for my game. (Starting code with one player then will add player 2).<br>
-Player 1: Woodard and Player 2: Garcia both shoot a three pointer and either make it or miss it.<br>
-A make is worth 3 points; A miss is worth 0 points. 
-The player with the most points wins and is the MVP!</p>
+<p>
+Player 1: Lynette Woodard and Player 2: Robbie Garcia both shoot a three pointer from the top of the key. Each player will either make their shot or miss their shot. A made shot is worth THREE points; a missed shot is worth ZERO points. The player with the least points is Knocked Out and the player with the most points Wins the game and is the MVP! If the game ends in a tie, the game goes into one overtime. If at the end of overtime the game is still tied, both players are crowned MVP.</p>
 
 <h2>Results</h2>
-    <p>
-        Player 1, you have <?php echo $totalPlayer1; ?> points and you are not the MVP Winner this time.
+    <p>Player 1, you have <?php echo $totalPlayer1; ?> points and you are <strong>Knocked Out.</strong>
 <br>
-        Player 2, you have <?php echo $totalPlayer2; ?> points and you are the MVP Winner!
+        Player 2, you have <?php echo $totalPlayer2; ?> points and you are the <strong>Winner and MVP!</strong>
     </p>
-<br>
+    <h3>Thanks for Playing the Trailblazers MVP Knockout 3-Pt Shooting Game!</h3>
+
 <footer>
-<p>Thanks for Playing the Trailblazers Knockout 3-Pt Shooting Game!</p>
+    
+    <p>MVP ~ The Minimum Viable Product for The Most Valuable Player</p>
+    <br>
+<strong><a href="https://hesweb.dev/e2" target="_blank" title="Click to visit Harvard Extension School: Web Programming for Beginners with PHP">Harvard University Extension School: DGMD E-2 Web Programming with PHP
+          </a></strong>
 </footer>
+<br>
 </body>
 </html>
