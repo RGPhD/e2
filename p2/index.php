ed5103074e7f0a140dba8e3ba609191498c3b641
Robelyn Garcia <rog814@g.harvard.edu>
2019-09-28 00:59:50 -0700

    Modified meta tags and mechanics.

--- p2/index.php
+++ p2/index.php
@@ -21,12 +21,15 @@ if ($player1 > $player2) {
 
 <!DOCTYPE html>
 <html lang='en'>
+
 <head>
 
-    <title>Trailblazers MVP Knockout 3-Pt Shooting Game.</title>
-    <meta charset='utf-8'>
+  <title>Trailblazers MVP Knockout 3-Pt Shooting Game.</title>
+  <meta charset='utf-8'>
+  <meta name="description" content="Basic PHP Basketball Game Simulator APP">
+  <meta name="author" content="Robelyn Garcia">
 
-<!-- Create external CSS file? -->
+  <!-- Create external CSS file? -->
 <style>
 
       h1 {
@@ -74,34 +77,43 @@ if ($player1 > $player2) {
 
 <body>
 
-<header>
-<h1>Knockout 3-Pt Shooting Game <br>
-    Trailblazers MVP Version 1.16.15 beta</h1>
-<!-- Minimum Viable Product/Most Valuable Player --> 
-<img alt='WBCBL Trailblazers Basketball Player Logo' src='https://pbs.twimg.com/media/CBfUlbsVAAANjUG?format=jpg' class="center">
-</header>
+  <header>
+    <h1>Knockout 3-Pt Shooting Game <br> Trailblazers MVP Version 1.16.15 beta</h1>
+    <!-- Minimum Viable Product/Most Valuable Player -->
+    <img alt='WBCBL Trailblazers Basketball Player Logo' src='https://pbs.twimg.com/media/CBfUlbsVAAANjUG?format=jpg' class="center">
+  </header>
 
-<h2>Mechanics</h2>
-<p>
-Player 1: Lynette Woodard and Player 2: Robbie Garcia both shoot a three pointer from the top of the key. Each player will either make their shot or miss their shot. A made shot is worth THREE points; a missed shot is worth ZERO points. The player with the least points is Knocked Out and the player with the most points Wins the game and is the MVP!  If the game ends in a tie, the game goes into overtime. If at the end of overtime the game is still tied, the game continues in overtime rounds until one player wins and is crowned MVP.</p>
+  <h2>Mechanics</h2>
+  <ul>
+    <li>Player 1: Lynette Woodard and Player 2: Robbie Garcia both shoot a three pointer from the top of the key.</li>
+    <li>Each player will either make their shot or miss their shot.</li>
+    <li>A made shot is worth THREE points; a missed shot is worth ZERO points.</li>
+    <li>The player with the least points is Knocked Out and the player with the most points Wins the game and is the MVP!</li>
+    <li>If the game ends in a tie, the game goes into Overtime.</li>
+    <li>If at the end of Overtime the game is still tied, the game continues in Overtime rounds until one player wins and is crowned MVP.</li>
+    <li>Players points are noncumulative.</li>
+  </ul>
 
-<h2>Results</h2>
-<ul>
-    <li>Player 1, you have <?php echo $player1; ?> points.</li>
-    <li>Player 2, you have <?php echo $player2; ?> points.</li>
-    <li><strong>The winner is: </strong><?php echo $winner; ?>
-    </ul>
+  <h2>Results</h2>
+  <ul>
+    <li>Player 1, you have
+      <?php echo $player1; ?> points.</li>
+    <li>Player 2, you have
+      <?php echo $player2; ?> points.</li>
+    <li><strong>The winner is: </strong>
+      <?php echo $winner; ?>
+  </ul>
 
+  <h3>Thanks for Playing the Trailblazers MVP Knockout 3-Pt Shooting Game!</h3>
 
-    <h3>Thanks for Playing the Trailblazers MVP Knockout 3-Pt Shooting Game!</h3>
+  <footer>
 
-<footer>
-    
     <p>MVP ~ The Minimum Viable Product for The Most Valuable Player</p>
     <br>
-<strong><a href="https://hesweb.dev/e2" target="_blank" title="Click to visit Harvard Extension School: Web Programming for Beginners with PHP">Harvard University Extension School: DGMD E-2 Web Programming with PHP
+    <strong><a href="https://hesweb.dev/e2" target="_blank" title="Click to visit Harvard Extension School: Web Programming for Beginners with PHP">Harvard University Extension School: DGMD E-2 Web Programming with PHP
           </a></strong>
-</footer>
-<br>
+  </footer>
+  <br>
 </body>
+
 </html>
