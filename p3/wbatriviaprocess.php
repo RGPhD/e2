fff1fe8f9da9bc70b8eb89478e68beb8d150874f
Robelyn Garcia <rog814@g.harvard.edu>
2019-10-08 10:58:29 -0700

    Fixing error due to nesting a within a button

--- p3/wbatriviaprocess.php
+++ p3/wbatriviaprocess.php
@@ -53,7 +53,7 @@
       <li class='feedback'>You are incorrect. But you can learn all about the WBA Founder by listening to his <a href="https://www.amazon.com/Vision-Untold-Basketball-Association-Updated/dp/B07SF7YM2D" target="_blank" title="Click to visit the WBA Book Page on Amazon"> Audio Book</a> or seeing his <a href="https://www.wbathevision.com/the-vision-is-coming-to-kansas-city" target="_blank" title="Click to visit the WBA Vision Movie Page"> Film</a>!</li>
       </ul>
     <div>
-      <button class='refresh'><a class='refresh' title="Return Home and Play Again" href="http://p3.robelyngarcia.me">Click to Return Home and or Play Again</a></button>
+      <a class='refresh' title="Return Home and Play Again" href="http://p3.robelyngarcia.me">Click to Return Home and or Play Again</a>
     </div>
     <h3>Thanks for Playing the WBA Fantasy Basketball Game!</h3>
     
