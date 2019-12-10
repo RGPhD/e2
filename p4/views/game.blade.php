9e9609b9f0c00e583cd6b4405ebdc20392f8aa20
Robelyn Garcia <rog814@g.harvard.edu>
2019-12-09 19:46:30 -0700

    Fixing game view

--- p4/views/game.blade.php
+++ p4/views/game.blade.php
@@ -7,18 +7,22 @@ Individual Games - Game {{ $game['id'] }}
 @section('content')
 
   <h2>Game {{$game['id']}} Individual Result:</h2>
+  &rarr; <a href='/games'>P4 Game History</a>
   <ul>
 <li> Player picked {{ $game['player'] }}. </li>
 <li> Computer picked {{ $game['computer'] }}. </li>
 @if ($game['winner'] == 1)
 <li>         Player Won!
 @else ($game['winner'] == 2)
-         Player did not win.
-@endif
+<li> Player did not win. </li>
+<li> View all of <a href='/games'>P4 Game Results</a>
 </li>
+@endif
 </ul>
-
-  <p>&rarr; <a href='/games'>View all of P4 Game History</a></p>
-
-
+<br>
+<h2>The WBA is being inducted into the Black Archives of Mid-America</h2>
+The WBA League, Players, Coaches and Management will be inducted into the <a href='http://www.blackarchives.org/'>The Black Archives of Mid-America Museum</a> during Black History Month in Febuary, 2020. For tickets or more information contact Lightning Ned Mitchell at info@wbathevision.com and or Dr. Carma at 816-221-1600.
+<br>
+<br>
+<br>
 @endsection
